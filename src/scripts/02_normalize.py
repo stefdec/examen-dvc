56e9cb0ea614708c272575e8418fc2d20263b525
stefdec <stefdec@gmail.com>
2026-08-19 18:00:53 +0300

    chore:create normalize script

---
+++ src/scripts/02_normalize.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+from utils.utils import save_dataframe
+
+
+def normalize_data(df):
+    # Normalize the features using StandardScaler
+    scaler = StandardScaler()
+    normalized_feats = scaler.fit_transform(df)
+    normalized_df = pd.DataFrame(normalized_feats, columns=df.columns)
+    return normalized_df
+
+
+if __name__ == "__main__":
+    # Load the split data
+    X_train = pd.read_csv("data/processed_data/X_train.csv")
+    X_test = pd.read_csv("data/processed_data/X_test.csv")
+
+    # Normalize the features
+    X_train_scaled = normalize_data(X_train)
+    X_test_scaled = normalize_data(X_test)
+
+    # Save the normalized data to CSV files
+    save_dataframe(X_train_scaled, "X_train_scaled.csv", "data/processed_data")
+    save_dataframe(X_test_scaled, "X_test_scaled.csv", "data/processed_data")
