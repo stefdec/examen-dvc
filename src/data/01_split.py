34e3bf5a7a7ad7cd9f308729ab9048dead040f7c
stefdec <stefdec@gmail.com>
2026-08-19 18:53:32 +0300

    Run fill dvc pipeline

--- src/data/01_split.py
+++ src/data/01_split.py
@@ -9,6 +9,18 @@ sys.path.append(str(Path(__file__).resolve().parent.parent))
 from utils.utils import save_dataframe
 
 
+def process_date(df):
+    # Convert the 'date' column to datetime format
+    df["date"] = pd.to_datetime(df["date"])
+    # Extract year, month, and day from the 'date' column
+    df["year"] = df["date"].dt.year
+    df["month"] = df["date"].dt.month
+    df["day"] = df["date"].dt.day
+    # Drop the original 'date' column
+    df.drop("date", axis=1, inplace=True)
+    return df
+
+
 def split_data(df):
     # Split data into training and testing sets
     target = df["silica_concentrate"]
@@ -24,6 +36,7 @@ if __name__ == "__main__":
     df = pd.read_csv("data/raw_data/raw.csv")
 
     # Split the data into training and testing sets
+    df = process_date(df)
     X_train, X_test, y_train, y_test = split_data(df)
 
     # Save the split data to CSV files
